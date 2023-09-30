2cc734fa3e209b5322e932da852cb0997f291323
suruchi1997 <106206722+suruchi1997@users.noreply.github.com>
2023-09-29 21:38:19 -0700

    Add files via upload

---
+++ csv_com.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import scipy.stats as stats
+
+df_csv_1=pd.DataFrame()
+
+csv_files=[1,2,3]
+
+for file in csv_files:
+    # add the path to your csv files
+    df3=pd.read_csv("conv_mul/"+str(file)+".csv")
+
+    df_csv_1 = df_csv_1.append(df3, ignore_index=True)
+df_combined= df_csv_1.groupby(['beta'])["beta","success","lat_cost_mean","lat_cost_std","ctrl_cost","ctrl_cost_std","diff_cost","diff_cost_std","log_min_svd","log_max_svd"].mean()
+df_combined.to_html("Tab.html")
+df_combined.to_csv("out.csv")
